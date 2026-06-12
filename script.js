122a959115c1f3fd7f8577ea195af802209d8177
Yashasvi Paliwal <yashasvipaliwal2@gmail.com>
2026-06-12 13:38:50 +0530

    Add files via upload
    "added project files"

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio website loaded successfully!");
+
+// Ek simple function jo user ke button click karne par trigger hoga
+function sayHello() {
+    alert("Hello! Thanks for visiting my portfolio.");
+}
